b5f31197688d790baf8582025adb9b817b841beb
Feng Xu <fengxuu@amazon.com>
2023-12-05 17:33:53 +0800

    add missing file

---
+++ Index.py
@@ -0,0 +1,16 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Intelligent BI",
+    page_icon="👋",
+)
+
+st.write("## Welcome to Intelligent BI Playground! 👋")
+
+st.sidebar.success("Select a demo above.")
+
+st.markdown(
+    """
+    TBD
+"""
+)
